727074e3c399e83d359366e4ba994139e7ca0ac0
sarsur9 <rami.moon@outlook.com>
2024-08-20 22:03:59 +0300

    CRUD done

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import userRoute from "./routes/user.js";
 import authRoute from "./routes/auth.js";
 import mongoose from "mongoose"
 import { default as productRoute } from "./routes/product.js";
-import { default as cartRoute } from "./routes/product.js";
+import { default as cartRoute } from "./routes/cart.js";
+import { default as orderRoute } from "./routes/order.js";
 
 
 dotenv.config();
@@ -22,7 +23,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
-app.use("/api/cart", cartRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("Backend server running!");

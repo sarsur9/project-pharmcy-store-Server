7c50c9ed93b45d0d7fd6cc3d0af6e8ecac055b06
sarsur9 <rami.moon@outlook.com>
2024-08-26 21:49:00 +0300

    data inserting finish

--- models/Product.js
+++ models/Product.js
@@ -3,12 +3,16 @@ const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
+
     img: { type: String, required: true },
+
+    cat: { type: String, required: true },
+
     categories: { type: Array },
 
     size: { type: Array },
     price: { type: Number, required: true },
-        inStock: { type: Boolean, default: true },
+    inStock: { type: Boolean, default: true },
 
   },
   { timestamps: true }
